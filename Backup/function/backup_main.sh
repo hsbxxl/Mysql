6c7dde669742acf63f7bfd5bbffa95c46d394ca1
hsbxxl <42596688+hsbxxl@users.noreply.github.com>
2018-10-11 18:46:11 +0800

    .
    .

--- Backup/function/backup_main.sh
+++ Backup/function/backup_main.sh
@@ -3,7 +3,7 @@
 ../conf/config.sh
 
 # Prepare the related path for the backup
-function prepare_server_env{
+function prepare_server_env {
 if [ ! -d "$server_backup_base" ];then
 mkdir -p {$server_backup_base,$server_backupset_dir,$server_backup_script,$server_backup_script_temp,$server_backup_log}
 else
@@ -19,14 +19,15 @@ mkdir -p {$client_backup_base,$client_backupset_temp,$client_backupset_dir,$clie
 fi
 EOF
 
-scp -o "StrictHostKeyChecking no" $server_backup_script_temp/create_path_script.sh ${client_user}@"${host_name}":$client_backupset_temp
-ssh ${client_user}@${host_name} 'sh $client_backup_log_temp/create_path_script.sh >/dev/null 2>&1 &' 
+scp -o "StrictHostKeyChecking no" $server_backup_script_temp/create_path_script.sh ${client_user}@"${host_name}":/tmp
+
+ssh ${client_user}@${host_name} "sh /tmp/create_path_script.sh; rm /tmp/create_path_script.sh" 
 
 #generate the backupset name, that is only valid in this function
 backupset_name=${host_name}_$(date +%F-%H-%M).tar.gz
 
 cat <<EOF > $server_backup_script_temp/backup_script.sh
-innobackupex --defaults-file=$mycnf --user=$db_user --password="$db_password" --parallel=4  --stream=tar $client_backupset_path 2>$client_backup_log_temp/${host_name}_$(date +%F-%H-%M)_output.log| gzip > $client_backupset_temp/$backupset_name
+innobackupex --defaults-file=$mycnf --user=$db_user --password="$db_password" --parallel=4  --stream=tar $client_backupset_dir 2>$client_backup_log_temp/${host_name}_$(date +%F-%H-%M)_output.log| gzip > $client_backupset_temp/$backupset_name
 #generate status log
 tail -1 $client_backup_log_temp/${host_name}_$(date +%F-%H-%M)_output.log| awk '{print \$3}' > $client_backup_log_temp/status.log
 EOF
@@ -54,8 +55,11 @@ do
     host_name=$line
 	echo $host_name
 
+#需要增加判断,host都是能正常访问的,如果不能访问,退出备份
+
+
 	# get the backup status
-backup_stat=`ssh ${client_user}@${host_name} 'cat $server_backup_log/status.log'`
+backup_stat=`ssh ${client_user}@${host_name} "cat $client_backup_log_temp/status.log"`
 
    if [ "$backup_stat" = "completed" ] && [ -n "$backup_stat" ]
    then
@@ -63,8 +67,13 @@ backup_stat=`ssh ${client_user}@${host_name} 'cat $server_backup_log/status.log'
       remote_backupset_size=`ssh ${client_user}@${host_name} "du -sm $client_backupset_temp/${host_name}_$(date +%F-)*.tar.gz"`
 	  echo $remote_backupset_size
     
-
 	  scp ${client_user}@${host_name}:$client_backupset_temp/${host_name}_$(date +%F-)*.tar.gz  $server_backupset_dir/$host_name/
+	  #mv the backup from temp path to dir
+	   ssh ${client_user}@${host_name} "mv $client_backupset_temp/${host_name}_$(date +%F-)*.tar.gz $client_backupset_dir"
+         #临时调试         
+		  echo $client_backupset_temp/${host_name}_$(date +%F-)*.tar.gz
+          echo $server_backupset_dir/$host_name/ 
+	  
 	  local_backupset_size=`du -sm $server_backupset_dir/$host_name/${host_name}_$(date +%F-)*.tar.gz`
 	  #If the size diff , print the info
 	  if [ "$remote_backupset_size" != "$local_backupset_size"];then 
@@ -77,15 +86,14 @@ backup_stat=`ssh ${client_user}@${host_name} 'cat $server_backup_log/status.log'
 	  echo ${host_name} >> $server_backup_log/ok.list
 	  sed -i '/'"$host_name"'/d' $server_backup_script_temp/scp_list
 	  #rename the output log,
-	  mv $client_backup_log_temp/${host_name}_$(date +%F)*_output.log $client_backup_log/${host_name}_$(date +%F)*_output.log
+	   ssh ${client_user}@${host_name} "mv $client_backup_log_temp/${host_name}_$(date +%F)*_output.log $client_backup_log"
+	   #临时调试
+	   ssh ${client_user}@${host_name} "ls $client_backup_log_temp/${host_name}_$(date +%F)*_output.log"
    else
       echo "The scp not completed!! The fail host is ${host_name}"
       echo ${host_name} >> $client_backupset_temp/fail.list
     fi
-else
-   echo "The output file is not exist. The backup not completed!!"
 
 done
-
 }
 
